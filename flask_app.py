2d54be632579c20ea03196af42717cab9988139d
bandanadee <bandanadee@24dcaea7d349>
2018-05-18 16:03:57 +0000

    -Login to track logged in users. Use Flask-Login to track logged in users.

--- flask_app.py
+++ flask_app.py
@@ -1,40 +1,25 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
-
 from flask import render_template
-
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_bootstrap import Bootstrap
-
 from flask_nav import Nav
-
 from flask_sslify import SSLify
-
 from flask_nav.elements import Navbar, Subgroup, View
 app = Flask(__name__)
-
 from flask_wtf import FlaskForm
-
 from wtforms import StringField, PasswordField, SubmitField
-
 from wtforms.validators import InputRequired, Email, Length
-
 from werkzeug.security import check_password_hash
-
 from werkzeug.security import generate_password_hash
-
 from flask import redirect
-
 from flask import url_for
-
 from wtforms.validators import ValidationError
-
+from flask import flash
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
 
 app.config.from_object('config.BaseConfig')
 db = SQLAlchemy(app)
+login = LoginManager(app)
 
 Bootstrap(app)
 
@@ -67,7 +52,12 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please choose a different username.')
 
-class User(db.Model):
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Sign in')
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15))
     email = db.Column(db.String(150))
@@ -79,6 +69,9 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+@login.user_loader
+def load_user(user_id):
+    return User.query.filter_by(id=int(user_id)).first()
 
 @app.route('/')
 def homepage():
@@ -105,10 +98,9 @@ def register():
         new_user.set_password(form.password.data)
         db.session.add(new_user)
         db.session.commit()
+        login_user(new_user)
         return redirect(url_for('homepage'))
     return render_template('register.html', form=form)
-
-
 
 @app.route('/top_ten_songs')
 def top_ten_songs():
@@ -116,10 +108,32 @@ def top_ten_songs():
     return render_template('top_ten_songs.html',
                             songs=songs)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('homepage'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user or not user.check_password(form.password.data):
+            flash('Username or password is incorrect.', 'danger')
+            return render_template('login.html', form=form)
+        login_user(user)
+        return redirect(url_for('homepage'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
+
+
 nav = Nav(app)
 @nav.navigation('mysite_navbar')
 def create_navbar():
     home_view = View('Home', 'homepage')
+    login_view = View('Login', 'login')
+    logout_view = View('Logout', 'logout')
     register_view = View('Register', 'register')
     about_me_view = View('About Me', 'about_me')
     class_schedule_view = View('Class Schedule', 'class_schedule')
@@ -128,7 +142,10 @@ def create_navbar():
                              about_me_view,
                              class_schedule_view,
                              top_ten_songs_view)
-    return Navbar('MySite', home_view, misc_subgroup, register_view)
+    if current_user.is_authenticated:
+        return Navbar('MySite', home_view, misc_subgroup, logout_view)
+    else:
+        return Navbar('MySite', home_view, misc_subgroup, login_view, register_view)
 
 if __name__ == '__main__':
   db.create_all()
